159fb4586463b8416de7828e19832c565a3b82d9
auroraptor <interestingbutno@yandex.ru>
2023-06-22 11:59:43 +0300

    fix: change logic in Graph component

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -3,12 +3,28 @@ import useGraph from "./hooks/useGraph";
 import useCreateColumns from "./hooks/useCreateColumns";
 import useMinimizeCrossings from "./hooks/useMinimizeCrossing";
 import GraphEdge from "./GraphEdge";
-import { Edge } from "./types/graph";
+import { Edge, GraphData } from "./types/graph";
 
-const Graph: React.FC<{ id: number }> = ({ id }) => {
+const createFakeGraphData = () => {
+  return {
+    nodes: [],
+    edges: [],
+  }
+}
+
+const Graph: React.FC<{ id: number }> = ({ id }): React.ReactElement | null => {
   const { graph, loading, error } = useGraph(id);
-  const columns = graph ? useCreateColumns(graph) : null;
-  columns && graph ? useMinimizeCrossings(columns, graph.edges) : null;
+
+  if (!graph) {
+    return null;
+  }
+
+  const columns = useCreateColumns(graph || createFakeGraphData());
+  useMinimizeCrossings(columns, graph.edges || []);
+
+  if (error) {
+    return <div>{`Oops! :( ${error.message}`}</div>;
+  }
 
   const [edges, setEdges] = useState<Edge[]>([]);
   const [renderEdges, setRenderEdges] = useState(false);
@@ -29,47 +45,41 @@ const Graph: React.FC<{ id: number }> = ({ id }) => {
     return <div>Loading...</div>;
   }
 
-  if (graph) {
-    return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          padding: "30px",
-        }}
-      >
-        {columns?.map((column, index) => (
-          <div key={index}>
-            <h2>Level {index + 1}</h2>
-            <ul
-              style={{ listStyle: "none", padding: "0", textAlign: "center" }}
-            >
-              {column.map((node) => (
-                <li
-                  key={node.id}
-                  id={`${node.id}`}
-                  style={{
-                    border: "2px solid black",
-                    borderRadius: "10px",
-                    padding: "6px 10px",
-                    margin: "10px 0",
-                  }}
-                >
-                  {node.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-        {renderEdges &&
-            edges.map((edge) => (
-              <GraphEdge key={edge.fromId + "-" + edge.toId} edge={edge} />
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: "30px",
+      }}
+    >
+      {columns?.map((column, index) => (
+        <div key={index}>
+          <h2>Level {index + 1}</h2>
+          <ul style={{ listStyle: "none", padding: "0", textAlign: "center" }}>
+            {column.map((node) => (
+              <li
+                key={node.id}
+                id={`${node.id}`}
+                style={{
+                  border: "2px solid black",
+                  borderRadius: "10px",
+                  padding: "6px 10px",
+                  margin: "10px 0",
+                }}
+              >
+                {node.name}
+              </li>
             ))}
-      </div>
-    );
-  }
-
-  return null;
+          </ul>
+        </div>
+      ))}
+      {renderEdges &&
+        edges.map((edge) => (
+          <GraphEdge key={edge.fromId + "-" + edge.toId} edge={edge} />
+        ))}
+    </div>
+  );
 };
 
 export default Graph;
