8dbac9d8ab9cccc4d97a8d73250531057efb7d0e
auroraptor <interestingbutno@yandex.ru>
2023-06-22 21:48:35 +0300

    chore: add global.fetch to jest setup configuration

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,3 +3,5 @@ import { server } from "./src/mocks/server";
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+
+global.fetch = require('jest-fetch-mock');
