96fd51afd7cfe231a162b2ddc48d69b336d73066
auroraptor <interestingbutno@yandex.ru>
2023-06-22 11:56:30 +0300

    fix: correct main.tsx to ensure proper rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
 if (process.env.NODE_ENV === "development") {
-  import("./mocks/browser.js").then(({ worker }) => worker.start());
+  import("./mocks/browser.js")
+    .then(({ worker }) => worker.start())
+    .then(() => {
+      const container = document.getElementById("root");
+      const root = createRoot(container!);
+      root.render(<App />);
+    });
+} else {
+  const container = document.getElementById("root");
+  const root = createRoot(container!);
+  root.render(<App />);
 }
-
-ReactDOM.render(<App />, document.getElementById("root"));
