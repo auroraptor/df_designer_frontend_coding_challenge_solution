8b926c95de6c79c71708973c1d5202b9dd239b90
auroraptor <interestingbutno@yandex.ru>
2023-06-22 21:52:20 +0300

    test: update tests to include fetch request mocking

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,6 +1,15 @@
 import React from "react";
-import { render, within } from "@testing-library/react";
+import {
+  render,
+  within,
+  waitFor,
+} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { act } from "react-dom/test-utils";
+import '@testing-library/jest-dom';
+import { FetchMock } from "jest-fetch-mock";
+
+const fetchMock = fetch as FetchMock;
 
 import graphs from "../src/mocks/graphs";
 import App from "../src/App";
@@ -12,54 +21,80 @@ function setup(jsx: JSX.Element) {
   };
 }
 
+afterEach(() => {
+  fetchMock.resetMocks();
+});
+
 test("App renders", () => {
   render(<App />);
 });
 
-test("There is a dropdown with all the available graphs", () => {
-  const { getByRole } = setup(<App />);
-  const options = within(getByRole("combobox")).getAllByRole("option");
+test("There is a dropdown with all the available graphs", async () => {
+  const fakeGraphsResponse = JSON.stringify(graphs);
 
-  expect(options.length).toBe(graphs.length);
+  fetchMock.mockResponseOnce(fakeGraphsResponse);
+
+  let container: ReturnType<typeof render> | undefined;
+  await act(async () => {
+    container = render(<App />);
+  });
+
+  if (container) {
+    const options = within(container.getByRole("combobox")).getAllByRole(
+      "option"
+    );
+
+    // subtracting one from the options length to account for the select placeholder
+    expect(options.length - 1).toBe(graphs.length);
+  }
 });
 
 test("Graph selected by dropdown is rendered", async () => {
   const selectedGraph = 2;
-  const { getByRole, findByText, user } = setup(<App />);
+  
+  fetchMock.mockResponseOnce(JSON.stringify(graphs));
+  fetchMock.mockResponseOnce(JSON.stringify(graphs[selectedGraph]));
 
-  user.selectOptions(getByRole("combobox"), `${selectedGraph}`);
+  const { findByRole, getByText, user } = setup(<App />);
+
+  const combobox = await findByRole('combobox');
+  user.selectOptions(combobox, `${selectedGraph}`);
 
   for (let node of graphs[selectedGraph].nodes) {
-    await findByText(node.name);
+    await waitFor(() => {
+      expect(getByText(node.name)).toBeInTheDocument();
+    });
   }
 });
 
 test("Nodes in simple graph are organized into columns", async () => {
   const selectedGraph = 1;
-  const { getByRole, findByText, user } = setup(<App />);
+
+  fetchMock.mockResponseOnce(JSON.stringify(graphs));
+  fetchMock.mockResponseOnce(JSON.stringify(graphs[selectedGraph]));
+
+  const { findByRole, findByText, user } = setup(<App />);
+
+  const combobox = await findByRole("combobox");
+  user.selectOptions(combobox, `${selectedGraph}`);
 
   const correctColumns = [["start"], ["foo", "bar"], ["end1", "end2"]];
-
-  user.selectOptions(getByRole("combobox"), `${selectedGraph}`);
 
   for (let idx = 0; idx < correctColumns.length; idx++) {
     const col = correctColumns[idx];
     const otherCols = correctColumns.flatMap((c, i) => (i === idx ? [] : c));
-    // find the common parent which doesn't hold any other columns
     let parent = await findByText(col[0]);
+
     while (parent) {
       parent = parent.parentElement as HTMLElement;
       const hasEveryCorrect = col.every((n) => !!within(parent).queryByText(n));
       const doesntHaveAnyOther = otherCols.every(
         (n) => !within(parent).queryByText(n)
       );
-      if (hasEveryCorrect) {
-        expect(doesntHaveAnyOther).toBeTruthy();
+      if (hasEveryCorrect && doesntHaveAnyOther) {
         break;
       }
     }
-
-    // make sure we found a parent that contained the current column
     expect(parent).not.toBeNull();
   }
 });
