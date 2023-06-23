de92626ceb691a5ee62e9cc64ba30edddfd209c0
auroraptor <interestingbutno@yandex.ru>
2023-06-23 15:46:21 +0300

    refactor: modify useGraph hook to handle null id

--- src/hooks/useGraph.ts
+++ src/hooks/useGraph.ts
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react";
 import { GraphData } from "../types/graph";
 
-const useGraph = (id: number) => {
-  const [graph, setGraph] = useState<GraphData>({
+const useGraph = (id: number | null) => {
+  const [graph, setGraph] = useState<GraphData | null>({
     nodes: [],
     edges: [],
   });
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
     const fetchGraph = async () => {
       try {
+        setLoading(true);
+        if (id === null) {
+          setGraph(null);
+          setLoading(false);
+          return;
+        }
         const response = await fetch(`/api/graphs/${id}`);
         const data: GraphData = await response.json();
         setGraph(data);
