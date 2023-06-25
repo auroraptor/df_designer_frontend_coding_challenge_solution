e65d31e8bc614726ef251d73a4c37adc4982da26
auroraptor <interestingbutno@yandex.ru>
2023-06-25 20:59:34 +0300

    feat: implement drag n drop for nodes

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -15,8 +15,14 @@ const createFakeGraphData = (): GraphData => {
 const Graph: React.FC<GraphProps> = ({ graph }): React.ReactElement | null => {
   const [edges, setEdges] = useState<Edge[]>([]);
   const [renderEdges, setRenderEdges] = useState(false);
-  const columns = useCreateColumns(graph || createFakeGraphData());
+  const [columns, setColumns] = useState(() =>
+    useCreateColumns(graph || createFakeGraphData())
+  );
   useMinimizeCrossings(columns, graph.edges || []);
+
+  useEffect(() => {
+    setColumns(useCreateColumns(graph || createFakeGraphData()));
+  }, [graph]);
 
   useEffect(() => {
     setEdges(graph.edges || []);
@@ -28,6 +34,42 @@ const Graph: React.FC<GraphProps> = ({ graph }): React.ReactElement | null => {
     }
   }, [edges]);
 
+  const handleDragStart = (event: React.DragEvent, nodeId: string) => {
+    event.dataTransfer.setData("nodeId", nodeId);
+  };
+
+  const handleDrop = (event: React.DragEvent, columnId: number) => {
+    event.preventDefault();
+    const nodeId = event.dataTransfer.getData("nodeId");
+  
+    let draggedNode;
+    let draggedNodeIndex;
+    let draggedNodeColumnIndex;
+    for (let i = 0; i < columns.length; i++) {
+      const index = columns[i].findIndex((node) => String(node.id) === nodeId);
+      if (index !== -1) {
+        draggedNode = columns[i][index];
+        draggedNodeIndex = index;
+        draggedNodeColumnIndex = i;
+        break;
+      }
+    }
+  
+    const newColumns = [...columns];
+    if (draggedNodeColumnIndex !== undefined && draggedNodeIndex !== undefined)
+    newColumns[draggedNodeColumnIndex].splice(draggedNodeIndex, 1);
+  
+
+    if (draggedNode !== undefined)
+    newColumns[columnId].push(draggedNode);
+  
+    setColumns(newColumns);
+  };
+
+  const handleDragOver = (event: React.DragEvent) => {
+    event.preventDefault();
+  };
+
   return (
     <div
       style={{
@@ -37,18 +79,26 @@ const Graph: React.FC<GraphProps> = ({ graph }): React.ReactElement | null => {
       }}
     >
       {columns?.map((column, index) => (
-        <div key={index}>
+        <div
+          key={index}
+          onDrop={(event) => handleDrop(event, index)}
+          onDragOver={handleDragOver}
+        >
           <h2>Level {index + 1}</h2>
           <ul style={{ listStyle: "none", padding: "0", textAlign: "center" }}>
             {column.map((node) => (
               <li
                 key={node.id}
                 id={`${node.id}`}
+                draggable
+                onDragStart={(event) => handleDragStart(event, String(node.id))}
                 style={{
                   border: "1px solid #4d4d4d",
                   borderRadius: "4px",
                   padding: "6px 12px",
                   margin: "10px 0",
+                  cursor: "move",
+                  zIndex: "-1"
                 }}
               >
                 {node.name}
